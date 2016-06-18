adbe025579429ba472b47c05df79fbf0625adc2e
= <sepand.haghighi@yahoo.com>
2016-06-18 19:13:40 +0430

    docs : comment

--- projects/RegEX/main.py
+++ projects/RegEX/main.py
@@ -3,21 +3,22 @@ import re
 source="asjdhjkqwhejkqgggwelkajsdkljzcnm,zcnakgsjdaa1Sd9jsalamjajsdkashdkjqwehqwkejahsdjzxckjhjkhzjckhasjdkhasd"
 
 if __name__=="__main__":
-    find_1=re.match(r"salam",source)
+    find_1=re.match(r"salam","salam") #Match Method
     print(find_1)
-    find_2=re.search(r"salam",source)
-    print(find_2.start())
-    print(find_2.end())
-    print(find_2.group())
-    print(find_2.span())
-    print(re.sub("salam","gh",source))
-    print(re.match(r"^gr.y","gray").group())
-    print(re.search(r"[A-Z][a-z][0-9]",source).group())
-    print(re.search(r"[^A-Z][a-z][0-9]",source).group())
-    print(re.search(r"q(g)*",source).group())
-    print(re.search(r"q(g)+",source).group())
-    print(re.search(r"q(g)?",source).group())
-    match=re.match(r"(?P<first>abc)(?:def)(ghi)","abcdefghi")
+    find_2=re.search(r"salam",source) # Searh Method
+    print(find_2.start()) # Start Index
+    print(find_2.end()) # Last Index
+    print(find_2.group()) # Groups
+    print(find_2.span()) # Span Index
+    print(re.sub("salam","gh",source)) # Sub method and return a string
+    print(re.match(r"^gr.y","gray").group()) # ^start metacharacter and . meta character
+    print(re.search(r"[A-Z][a-z][0-9]",source).group()) #Character Classes
+    print(re.search(r"[^A-Z][a-z][0-9]",source).group()) # Not Of Character Classes
+    print(re.search(r"q(g)*",source).group()) # * metacharacter --> 0 or more of that group
+    print(re.search(r"q(g)+",source).group()) # + metacharacter --> 1 or more of that group
+    print(re.search(r"q(g)?",source).group()) # ? metacharacter --> 0 or 1 of that group
+    match=re.match(r"(?P<first>abc)(?:def)(ghi)","abcdefghi") # name group (?P<group_name>group) and unattached group (?:group)
     print(match.group("first"))
     print(match.groups())
-    print(re.findall(r"k(q|g)",source))
+    print(re.findall(r"k(q|g)",source)) # (|) or metacharacter
+    
